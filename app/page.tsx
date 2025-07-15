b9c3700ffac3775acaa8a54007354f7457864e70
genius-ayush <ayushsanjayrawal@gmail.com>
2025-07-15 19:44:35 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-10"><Button>signup</Button></div>
+  );
+}
